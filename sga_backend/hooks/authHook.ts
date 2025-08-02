44895031cf79953bf8e78df3a6b21c254b5ff1f9
Ali Salem Essouiah <contact@aliess.me>
2025-08-02 01:25:52 +0100

    checkpoint

--- sga_backend/hooks/authHook.ts
+++ sga_backend/hooks/authHook.ts
@@ -4,6 +4,8 @@ import * as jwt from "jsonwebtoken";
 import { handleError } from "../core/utils/errorHandler";
 import { HttpStatusCode } from "../core/enums/response/httpStatusCode";
 import { errorResponse } from "../core/resources/response/localizedErrorResponse";
+import { UserCredential } from "@prisma/client";
+import { ResponseLanguage } from "../core/enums/response/responseLanguage";
 
 const envApiKey = process.env.API_SECRET_KEY;
 const jwtSecretToken = process.env.JWT_SECRET_ACCESS;
@@ -13,49 +15,37 @@ export async function integrationAuthHook(
   reply: FastifyReply
 ) {
   const apiKey = request.headers["apikey"];
-  const language = request.headers["language"] as string;
 
   try {
     if (!apiKey) {
       throw new HttpError(
         HttpStatusCode.UNAUTHORIZED,
-        errorResponse(language).errorTitle.LACK_OF_CREDENTIALS,
-        errorResponse(language).errorMessage.LACK_OF_CREDENTIALS
+        errorResponse(ResponseLanguage.ARABIC).errorTitle.LACK_OF_CREDENTIALS,
+        errorResponse(ResponseLanguage.ARABIC).errorMessage.LACK_OF_CREDENTIALS
       );
     }
 
     if (apiKey !== envApiKey) {
       throw new HttpError(
         HttpStatusCode.UNAUTHORIZED,
-        errorResponse(language).errorTitle.INVALID_CREDENTIALS,
-        errorResponse(language).errorMessage.INVALID_CREDENTIALS
+        errorResponse(ResponseLanguage.ARABIC).errorTitle.INVALID_CREDENTIALS,
+        errorResponse(ResponseLanguage.ARABIC).errorMessage.INVALID_CREDENTIALS
       );
     }
-
-    request.body = {
-      ...(request.body || {}),
-      language,
-      userId: null,
-      isAdmin: null,
-    };
   } catch (error) {
-    return handleError(error, reply, language);
+    return handleError(error, reply, ResponseLanguage.ARABIC);
   }
 }
 
-export async function authHook(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
-  const language = request.headers["language"] as string;
+export async function authHook(request: FastifyRequest, reply: FastifyReply) {
   const accessToken = request.headers["authorization"];
 
   try {
     if (!accessToken) {
       throw new HttpError(
         HttpStatusCode.UNAUTHORIZED,
-        errorResponse(language).errorTitle.AUTHENTICATION_ERROR,
-        errorResponse(language).errorMessage.LACK_OF_CREDENTIALS,
+        errorResponse(ResponseLanguage.ARABIC).errorTitle.AUTHENTICATION_ERROR,
+        errorResponse(ResponseLanguage.ARABIC).errorMessage.LACK_OF_CREDENTIALS,
         { accessUnauthorized: true }
       );
     }
@@ -63,8 +53,10 @@ export async function authHook(
     if (!jwtSecretToken) {
       throw new HttpError(
         HttpStatusCode.INTERNAL_SERVER_ERROR,
-        errorResponse(language).errorTitle.INTERNAL_SERVER_ERROR,
-        errorResponse(language).errorMessage.INTERNAL_SERVER_ERROR,
+        errorResponse(ResponseLanguage.ARABIC).errorTitle.INTERNAL_SERVER_ERROR,
+        errorResponse(
+          ResponseLanguage.ARABIC
+        ).errorMessage.INTERNAL_SERVER_ERROR,
         { accessUnauthorized: true }
       );
     }
@@ -76,8 +68,10 @@ export async function authHook(
       if (err instanceof jwt.TokenExpiredError) {
         throw new HttpError(
           HttpStatusCode.UNAUTHORIZED,
-          errorResponse(language).errorTitle.ACCESS_TOKEN_EXPIRED,
-          errorResponse(language).errorMessage.EXPIRED_TOKEN,
+          errorResponse(
+            ResponseLanguage.ARABIC
+          ).errorTitle.ACCESS_TOKEN_EXPIRED,
+          errorResponse(ResponseLanguage.ARABIC).errorMessage.EXPIRED_TOKEN,
           { expiredAccessToken: true }
         );
       }
@@ -86,12 +80,11 @@ export async function authHook(
 
     request.body = {
       ...(request.body || {}),
-      language,
       userId: decoded.userId,
       isAdmin: decoded.isAdmin,
     };
   } catch (error) {
-    return handleError(error, reply, language);
+    return handleError(error, reply, ResponseLanguage.ARABIC);
   }
 }
 
@@ -99,14 +92,13 @@ export async function isAdminHook(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const language = request.headers["language"] as string;
   const userId = (request.body as any).userId;
 
   try {
     const user = await request.server.prisma.user.findUnique({
       where: {
         id: userId,
-        credential: "standard",
+        credential: UserCredential.admin,
         deletedAt: null,
       },
     });
@@ -114,36 +106,12 @@ export async function isAdminHook(
     if (!user) {
       throw new HttpError(
         HttpStatusCode.FORBIDDEN,
-        errorResponse(language).errorTitle.UNAUTHORIZED_ACCESS,
-        errorResponse(language).errorMessage.UNAUTHORIZED_ACCESS,
+        errorResponse(ResponseLanguage.ARABIC).errorTitle.UNAUTHORIZED_ACCESS,
+        errorResponse(ResponseLanguage.ARABIC).errorMessage.UNAUTHORIZED_ACCESS,
         { accessUnauthorized: true }
       );
     }
   } catch (error) {
-    return handleError(error, reply, language);
-  }
-}
-
-export async function isVerifiedHook(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
-  const language = request.headers["language"] as string;
-  const userId = (request.body as any).userId;
-
-  try {
-    const user = await request.server.prisma.user.findUnique({
-      where: { id: userId,  deletedAt: null },
-    });
-
-    if (!user) {
-      throw new HttpError(
-        HttpStatusCode.FORBIDDEN,
-        errorResponse(language).errorTitle.EMAIL_NOT_VERIFIED,
-        errorResponse(language).errorMessage.EMAIL_NOT_VERIFIED
-      );
-    }
-  } catch (error) {
-    return handleError(error, reply, language);
+    return handleError(error, reply, ResponseLanguage.ARABIC);
   }
 }
