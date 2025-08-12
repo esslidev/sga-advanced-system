b4fd0365fcbb64bf767be6ec33a9441b0d617a65
ali salem essouiah <contact@aliess.me>
2025-08-12 20:04:57 +0200

    checkpoint

--- sga_frontend/src/features/pages/Login/Login.tsx
+++ sga_frontend/src/features/pages/Login/Login.tsx
@@ -17,40 +17,46 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="loginPage">
-      <div id="loginContainer">
-        <h2 className="title">تسجيل الدخول</h2>
-
-        <div className="inputs">
-          <CustomTextInput
-            name="رقم البطاقة الوطنية"
-            type="text"
-            value={CIN}
-            onChange={(e) => setCIN(e.target.value)}
-          />
-          <CustomTextInput
-            name="كلمة السر"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        <div className="side-image col-md-5" />
+        <div className="login-form col-md-7">
+          <div className="from-header"></div>
+          <div className="margin with bootstrap">
+            <div className="form-title">
+              <p className="title">تسجيل الدخول</p>
+              <p className="sub-title"></p>
+            </div>
+            <div className="form-inputs">
+              <CustomTextInput
+                name="رقم البطاقة الوطنية"
+                type="text"
+                value={CIN}
+                onChange={(e) => setCIN(e.target.value)}
+              />
+              <CustomTextInput
+                name="كلمة السر"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {response && (
+              <p
+                className="error-response"
+                style={{ color: "red", marginTop: "1rem" }}
+              >
+                {response.message}
+              </p>
+            )}
+            <CustomButton
+              name={loading ? "جاري الدخول..." : "تسجيل الدخول"}
+              isInsert
+              disabled={loading}
+              onClick={handleLogin}
+            />
+          </div>
         </div>
-
-        {response && (
-          <p
-            className="error-message"
-            style={{ color: "red", marginTop: "1rem" }}
-          >
-            {response.message}
-          </p>
-        )}
-
-        <CustomButton
-          name={loading ? "جاري الدخول..." : "تسجيل الدخول"}
-          isInsert
-          disabled={loading}
-          onClick={handleLogin}
-        />
       </div>
     </div>
   );
